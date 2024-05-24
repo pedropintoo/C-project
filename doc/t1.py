7dd611548ca1198977de2ca61684e72d599585eb
Pedro Pinto <pmap@ua.pt>
2024-05-24 16:40:52 +0100

    working for all figures

--- doc/t1.py
+++ doc/t1.py
@@ -18,16 +18,16 @@ if __name__ == "__main__":
 
     top = Toplevel(root)
 
-    v0 = 601
+    v0 = 401
     v1 = v0
     v2 = v1
-    v3 = 601
+    v3 = 401
     v4 = v3
     v5 = v4
-    v6 = "Illustrating the minimum level graphical models"
+    v6 = "Illustrating the rectangle shape"
     v7 = v6
     v8 = v7
-    v9 = "alice blue"
+    v9 = "wheat"
     v10 = v9
     v11 = v10
 
@@ -40,73 +40,77 @@ if __name__ == "__main__":
         )
     last_view = v12
     view = v12
-    v13 = 200
-    v14 = v13
-    v15 = v14
-    cellSize = v15
-    v17 = cellSize
-    v18 = - v17
-    v19 = cellSize
-    v16 = (v18 , v19)
-    v21 = 50
-    v22 = 50
-    v20 = (v21 , v22)
-    v23 = v20
-    v24 = v23
-    v25 = "red"
-    v26 = v25
-    v27 = v26
+    v14 = 10
+    v15 = 0
+    v13 = (v14 , v15)
+    v17 = 200
+    v18 = 60
+    v16 = (v17 , v18)
+    v19 = v16
+    v20 = v19
+    v21 = "blue"
+    v22 = v21
+    v23 = v22
 
-    v28 = Line(
+    v24 = Rectangle(
         view=last_view
-        ,origin=v16
-        ,length=v24
-        ,fill=v27
+        ,origin=v13
+        ,length=v20
+        ,fill=v23
         )
-    last_view.add_object(v28)    
-     
-    v29 = 0.5
-    while (time.time() - last_refresh <= v29):
-        time.sleep(REFRESH_RATE)   
-
-    last_refresh = time.time()
-    view.update()
-    v31 = 0
-    v32 = cellSize
-    v30 = (v31 , v32)
-    v34 = 50
-    v35 = 50
-    v33 = (v34 , v35)
-    v36 = v33
+    last_view.add_object(v24)    
+    v26 = 0
+    v27 = 0
+    v25 = (v26 , v27)
+    v29 = 50
+    v30 = 50
+    v28 = (v29 , v30)
+    v31 = v28
+    v32 = v31
+    v33 = "pink"
+    v34 = v33
+    v35 = v34
+    v36 = 30
     v37 = v36
-    v38 = "orange"
-    v39 = v38
+    v38 = v37
+    v39 = 300
     v40 = v39
+    v41 = v40
 
-    v41 = Rectangle(
+    v42 = PieSlice(
         view=last_view
-        ,origin=v30
-        ,length=v37
-        ,fill=v40
+        ,origin=v25
+        ,length=v32
+        ,start=v38
+        ,extent=v41
+        ,fill=v35
         )
-    last_view.add_object(v41)    
-     
-    v42 = 0.5
-    while (time.time() - last_refresh <= v42):
-        time.sleep(REFRESH_RATE)   
-
+    last_view.add_object(v42) 
+    pacman = v42
     last_refresh = time.time()
     view.update()
     v43 = 1
     v44 = 10
     for i in range(v43, v44, 1):
-        v45 = "pedro "
-        print(v45)
-        v46 = i
-        print(v46)
-    v47 = last_view.waitClick()
-    v48 = v47
+         
+        v45 = 20
+        while (time.time() - last_refresh <= v45/1000):
+            time.sleep(REFRESH_RATE)   
+
+        last_refresh = time.time()
+        view.update()
+         
+        v46 = 25
+        while (time.time() - last_refresh <= v46/1000):
+            time.sleep(REFRESH_RATE)   
+
+        last_refresh = time.time()
+        view.update()
+        last_refresh = time.time()
+        view.update()
+    v47 = "Press any mouse button to quit"
+    print(v47)
+    v48 = last_view.waitClick()
     v49 = v48
-    p = v49
-    v50 = p
-    print(v50)
+    v50 = v49
+    pos = v50
