afedbb701c070a17b773d598a32e15967e0b7684
Cheiivan <giovanni.s@ua.pt>
2024-05-24 01:38:51 +0100

    class view

---
+++ doc/view.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+class View:
+
+    def __init__(self, height, width, background, title, Ox, Oy):
+        self.height = height
+        self.width = width
+        self.background = background
+        self.title = title
+        self.x = Ox
+        self.y = Oy
+
+    #não consegui terminar, vou continuar logo
+
+    
