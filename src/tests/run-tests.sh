a58d065888c44ba73ce4acd35e2d5f750a98be14
Pedro Pinto <pmap@ua.pt>
2024-06-04 12:37:49 +0100

    ad tests

--- src/tests/run-tests.sh
+++ src/tests/run-tests.sh
@@ -7,18 +7,29 @@ else
     exit 1
 fi
 
-echo "Building..."
-    antlr4-build 2>&1 > /dev/null
+echo "Building.."
+antlr4-build 2>&1 > /dev/null
 
+echo "tests/ValidTests:"
 for file in tests/ValidTests/*.agl; do
     #in green if output has last line "Semantic check passed." or red otherwise
     if cat $file | antlr4-run 2>&1 | grep -q "Semantic check passed."; then
-        echo -e "\e[32m $(basename "$file"): OK\e[0m"
+        echo "\e[32m $(basename "$file"): OK\e[0m"
     else
-        echo -e "\e[31m $(basename "$file"): FAIL\e[0m"
+        echo "\e[31m $(basename "$file"): FAIL\e[0m"
+    fi
+done
+
+echo "tests/InvalidTests:"
+for file in tests/InvalidTests/*.agl; do
+    #in green if output has last line "Semantic check failed." or red otherwise
+    if cat $file | antlr4-run 2>&1 | grep -q "Semantic check failed."; then
+        echo "\e[32m $(basename "$file"): OK\e[0m"
+    else
+        echo "\e[31m $(basename "$file"): FAIL\e[0m"
     fi
 done
 
 cd .. 
-echo "Cleaning up..."
+echo "Cleaning up.."
 antlr4-clean 2>&1 > /dev/null
