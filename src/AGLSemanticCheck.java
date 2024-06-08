7709afff65aa5b41f3ca96b8ea674d02a6aa138d
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-06-08 23:15:09 +0100

    now colors tested on tkinter

--- src/AGLSemanticCheck.java
+++ src/AGLSemanticCheck.java
@@ -20,32 +20,6 @@ public class AGLSemanticCheck extends AGLParserBaseVisitor<Boolean> {
    private final ObjectType viewType = new ObjectType("View");
    private final ObjectType modelType = new ObjectType("Model");
    private final EnumType enumType = new EnumType();
-   String[] COLORS = {
-    "snow", "ghost white", "white smoke", "gainsboro", "floral white", "old lace", "white", "black", "wheat",
-    "linen", "antique white", "papaya whip", "blanched almond", "bisque", "peach puff",
-    "navajo white", "lemon chiffon", "mint cream", "azure", "alice blue", "lavender",
-    "lavender blush", "misty rose", "dark slate gray", "dim gray", "slate gray",
-    "light slate gray", "gray", "light grey", "midnight blue", "navy", "cornflower blue", "dark slate blue",
-    "slate blue", "medium slate blue", "light slate blue", "medium blue", "royal blue",  "blue",
-    "dodger blue", "deep sky blue", "sky blue", "light sky blue", "steel blue", "light steel blue",
-    "light blue", "powder blue", "pale turquoise", "dark turquoise", "medium turquoise", "turquoise",
-    "cyan", "light cyan", "cadet blue", "medium aquamarine", "aquamarine", "dark green", "dark olive green",
-    "dark sea green", "sea green", "medium sea green", "light sea green", "pale green", "spring green",
-    "lawn green", "medium spring green", "green yellow", "lime green", "yellow green",
-    "forest green", "olive drab", "dark khaki", "khaki", "pale goldenrod", "light goldenrod yellow",
-    "light yellow", "yellow", "gold", "light goldenrod", "goldenrod", "dark goldenrod", "rosy brown",
-    "indian red", "saddle brown", "sandy brown",
-    "dark salmon", "salmon", "light salmon", "orange", "dark orange",
-    "coral", "light coral", "tomato", "orange red", "red", "hot pink", "deep pink", "pink", "light pink",
-    "pale violet red", "maroon", "medium violet red", "violet red",
-    "medium orchid", "dark orchid", "dark violet", "blue violet", "purple", "medium purple",
-    "thistle"
-};
-
-   String[] STATES = {
-      "normal", "hidden"
-   };
-
 
    @Override
    public Boolean visitProgram(AGLParser.ProgramContext ctx) {
@@ -1699,5 +1673,32 @@ public class AGLSemanticCheck extends AGLParserBaseVisitor<Boolean> {
 
       return false;
    }
+
+
+   String[] COLORS = {
+      "snow", "ghost white", "white smoke", "gainsboro", "floral white", "old lace", "white", "black", "wheat",
+      "linen", "antique white", "papaya whip", "blanched almond", "bisque", "peach puff",
+      "navajo white", "lemon chiffon", "mint cream", "azure", "alice blue", "lavender",
+      "lavender blush", "misty rose", "dark slate gray", "dim gray", "slate gray",
+      "light slate gray", "gray", "light grey", "midnight blue", "navy", "cornflower blue", "dark slate blue",
+      "slate blue", "medium slate blue", "light slate blue", "medium blue", "royal blue",  "blue",
+      "dodger blue", "deep sky blue", "sky blue", "light sky blue", "steel blue", "light steel blue",
+      "light blue", "powder blue", "pale turquoise", "dark turquoise", "medium turquoise", "turquoise",
+      "cyan", "light cyan", "cadet blue", "medium aquamarine", "aquamarine", "dark green", "dark olive green",
+      "dark sea green", "sea green", "medium sea green", "light sea green", "pale green", "spring green",
+      "lawn green", "medium spring green", "green yellow", "lime green", "yellow green",
+      "forest green", "green", "olive drab", "dark khaki", "khaki", "pale goldenrod", "light goldenrod yellow",
+      "light yellow", "yellow", "gold", "light goldenrod", "goldenrod", "dark goldenrod", "rosy brown",
+      "indian red", "saddle brown", "sandy brown",
+      "dark salmon", "salmon", "light salmon", "orange", "dark orange",
+      "coral", "light coral", "tomato", "orange red", "red", "hot pink", "deep pink", "pink", "light pink",
+      "pale violet red", "maroon", "medium violet red", "violet red",
+      "medium orchid", "dark orchid", "dark violet", "blue violet", "purple", "medium purple",
+      "thistle"
+  };
+  
+     String[] STATES = {
+        "normal", "hidden"
+     };
 
 }
